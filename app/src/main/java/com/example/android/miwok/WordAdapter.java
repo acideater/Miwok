ecc696bde012e5242af9db4362e2289ea7242dc3
AnaPaula <anapauladfp@gmail.com>
2018-05-24 22:07:48 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -0,0 +1,62 @@
+package com.example.android.miwok;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+    /*
+     * {@link AndroidFlavorAdapter} is an {@link ArrayAdapter} that can provide the layout for each list
+     * based on a data source, which is a list of {@link AndroidFlavor} objects.
+     * */
+   public class WordAdapter extends ArrayAdapter<Word>  {
+
+        /**
+         * Create a new {@link WordAdapter} object.
+         *
+         * @param context is the current context (i.e. Activity) that the adapter is being created in.
+         * @param words is the list of {@link Word}s to be displayed.
+         */
+
+        public WordAdapter(Activity context, ArrayList<Word> words) {
+            // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
+            // the second argument is used when the ArrayAdapter is populating a single TextView.
+            // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
+            // going to use this second argument, so it can be any value. Here, we used 0.
+            super(context, 0, words);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            // Check if the existing view is being reused, otherwise inflate the view
+            View listItemView = convertView;
+            if(listItemView == null) {
+                listItemView = LayoutInflater.from(getContext()).inflate(
+                        R.layout.list_item, parent, false);
+            }
+
+            // Get the {@link AndroidFlavor} object located at this position in the list
+            Word currentWord = getItem(position);
+
+            // Find the TextView in the list_item.xml layout with the ID version_name
+            TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
+            // Get the version name from the current AndroidFlavor object and
+            // set this text on the name TextView
+            miwokTextView.setText(currentWord.getMiwokTranslation());
+
+            // Find the TextView in the list_item.xml layout with the ID version_number
+            TextView defaultTextView = (TextView) listItemView.findViewById(R.id.default_text_view);
+            // Get the version number from the current AndroidFlavor object and
+            // set this text on the number TextView
+            defaultTextView.setText(currentWord.getDefaultTranslation());
+
+            // Return the whole list item layout (containing 2 TextViews and an ImageView)
+            // so that it can be shown in the ListView
+            return listItemView;
+        }
+
+}
